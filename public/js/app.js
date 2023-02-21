6c45d92a0ac676d2f4cb9a3d4518bc911f8ca1ff
Minh Lai Huynh Nhat <mlaihuynhnhat@openwt.com>
2023-02-21 10:11:48 +0700

    Adding basic styling for index

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
-fetch(url).then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
+const locationMessage = document.querySelector("#locationMessage");
+const resultMessage = document.querySelector("#forecastMessage");
+document.querySelector(".locationSearch").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = document.querySelector(".locationInput").value;
+  locationMessage.textContent = "";
+  resultMessage.textContent = "Loading...";
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          locationMessage.textContent = data.error;
+          resultMessage.textContent = "";
+        } else {
+          locationMessage.textContent = data.location;
+          resultMessage.textContent = data.forecast;
+        }
+      });
+    }
+  );
 });
