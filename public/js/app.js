d932af7ed8053b2aaceb6b2e2dbed32d122e5c13
Minh Lai Huynh Nhat <mlaihuynhnhat@openwt.com>
2023-02-21 14:02:43 +0700

    Update index ui with more informative information

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,28 @@
 const locationMessage = document.querySelector("#locationMessage");
-const resultMessage = document.querySelector("#forecastMessage");
+const resultMessage = document.querySelector("#temperatureMessage");
+const weatherMessage = document.querySelector("#weatherMessage");
+const weatherImage = document.querySelector(".weather-icon");
 document.querySelector(".locationSearch").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = document.querySelector(".locationInput").value;
   locationMessage.textContent = "";
   resultMessage.textContent = "Loading...";
+  weatherMessage.textContent = "";
+  weatherImage.setAttribute("src", "");
 
   fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         locationMessage.textContent = data.error;
         resultMessage.textContent = "";
+        weatherMessage.textContent = "";
+        weatherImage.setAttribute("src", "");
       } else {
         locationMessage.textContent = data.location;
         resultMessage.textContent = data.forecast;
+        weatherMessage.textContent = `The weather is ${data.weatherDescription.description}`;
+        weatherImage.setAttribute("src", data.weatherDescription.icon);
       }
     });
   });
